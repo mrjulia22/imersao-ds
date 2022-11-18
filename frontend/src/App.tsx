7ece584b6e1625738b5fcf0fe15f91ddb4bc4894
maria Julia <maria.jaraujo@ufpe.br>
2022-11-18 16:06:08 -0300

    ˜finalizacao do estilo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
+
 function App() {
   return (
-    <h1>Ola mundo</h1>
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
-
 }
 
 export default App
